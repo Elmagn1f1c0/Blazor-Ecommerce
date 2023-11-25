97279c62962aaa2634ed28b98d3d29184e453c15
Elmagn1f1c0 <steveamadi007@gmail.com>
2023-11-25 06:07:20 +0100

    Logout functionality

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -2,13 +2,13 @@ global using BlazorEcommerce.Shared;
 global using System.Net.Http.Json;
 global using BlazorEcommerce.Client.Services.ProductServices;
 global using BlazorEcommerce.Client.Services.CategoryService;
-using BlazorEcommerce.Client;
+global using BlazorEcommerce.Client.Services.AuthService;
+global using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 using BlazorEcommerce.Client.Services.CartService;
-using BlazorEcommerce.Client.Services;
-using BlazorEcommerce.Client.Services.AuthService;
+
 
 namespace BlazorEcommerce.Client
 {
@@ -26,6 +26,9 @@ namespace BlazorEcommerce.Client
             builder.Services.AddScoped<ICartService, CartService>();
             builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
             await builder.Build().RunAsync();
         }
